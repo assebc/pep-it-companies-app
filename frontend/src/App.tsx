65481111db8150cbfd0a49242b5b4a9f8d6974e7
assebc <claudioafbessa@gmail.com>
2023-01-24 21:49:26 +0000

    feat: modal create/edit companie

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import "./index.css";
-import {Space, Table, Layout, Button, Modal} from "antd"; 
+import {Space, Table, Layout, Button, Modal, Form, Input} from "antd"; 
 import type { ColumnsType } from "antd/es/table";
 import logo from "./assets/pep-logo.png";
+import TextArea from "antd/es/input/TextArea";
+import FormItem from "antd/es/form/FormItem";
 
-interface Empresas{
+interface Empresa{
   key: string;
   name: string;
   website_url: string;
@@ -12,41 +14,15 @@ interface Empresas{
   votes: number;
 }
 
-const columns: ColumnsType<Empresas> = [
-  {
-    title: "Nome",
-    dataIndex: "name",
-    key: "name",
-  },
-  { //FIXME: define max size per line
-    title: "Informações",
-    dataIndex: "reviews",
-    key: "reviews",
-  },
-  {
-    title: "Website",
-    dataIndex: "website_url",
-    key: "website_url",
-    render: text => <a>{text}</a>,
-  },
-  {
-    title: "Votos",
-    dataIndex: "votes",
-    key: "votes",
-  },
-  {
-    title: "Ações",
-    key: "actions",
-    render: (_,record) => (
-      <Space size="middle">
-          <a>Votar</a>
-          <a>Editar</a>
-      </Space>
-    ),
-  },
-];
+const default_state: Empresa = {
+  key: "",
+  name: "",
+  website_url: "",
+  reviews: "",
+  votes: 0,
+};
 
-const data: Empresas[] = [
+const data: Empresa[] = [
    {
      key: "1",
      name: "Critical Techworks",
@@ -74,17 +50,72 @@ const{Header,Content,Footer} = Layout;
 
 function App(){
 
+  const columns: ColumnsType<Empresa> = [
+    {
+      title: "Nome",
+      dataIndex: "name",
+      key: "name",
+    },
+    { //FIXME: define max size per line
+      title: "Informações",
+      dataIndex: "reviews",
+      key: "reviews",
+    },
+    {
+      title: "Website",
+      dataIndex: "website_url",
+      key: "website_url",
+      render: text => <a href={text} target="_blank">{text}</a>,
+    },
+    {
+      title: "Votos",
+      dataIndex: "votes",
+      key: "votes",
+    },
+    {
+      title: "Ações",
+      key: "actions",
+      render: (_,record) => { return (
+        <Space size="middle">
+            <Button>Votar</Button>
+            <Button onClick={() => showModalEdit(record)}>Editar</Button>
+        </Space>
+      )},
+    },
+  ];
+
+  const [title, setTitle] = useState("");
   const [open, setOpen] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
-  const [modalText, setModalText] = useState("");
+  const [infoName, setName] = useState("");
+  const [infoInfo, setInfo] = useState("");
+  const [infoURL, setURL] = useState("");
 
   const showModal = () => {
     setOpen(true);
+    setTitle("Adicionar uma empresa à tabela");
   };
 
+  const showModalEdit = (record: Empresa) => {
+    showModal();
+    setTitle("Editar empresa já existente");
+    setName(record.name);
+    setInfo(record.reviews);
+    setURL(record.website_url);
+  }
+
+  const resetFields = () => {
+    setName("");
+    setInfo("");
+    setURL("");
+  }
+
   const handleOk = () => {
-    setModalText("A adicionar empresa à tabela");
     setConfirmLoading(true);
+    setTimeout(() => {
+      setOpen(false);
+      setConfirmLoading(false);
+    }, 2000);
   };
 
   const handleCancel = () => {
@@ -94,34 +125,49 @@ function App(){
 
   return (
     <Layout>
-      <Header style={{display:"flex", alignItems:"center", justifyContent:"left", height:"10vh"}}>
-        <img style={{width:"100px", height:"100px"}}src={logo} alt="PEP LOGO"/>
+      <Header style={{height:"10vh", top:"0", width:"100%"}}>
+        <a href="https://www.twitch.tv/porqueeuprogramo" target="_blank"><img style={{width:"100px", height:"100px"}} src={logo} alt="PEP LOGO"/></a>
       </Header>
 
-      <Content>
-        <div style={{display:"flex", justifyContent:"center", alignItems:"center", padding:"24px"}}>
+      <Content style={{height:"84vh"}}>
+        <div style={{display:"flex", justifyContent:"center", alignItems:"center", padding:"64px 48px"}}>
 
           <div style={{display:"flex", justifyContent:"center", alignItems:"end", flexDirection:"column"}}>
-            <Button type="primary" size="large" onClick={showModal}>Criar</Button>
+            <Button type="primary" size="large" onClick={showModal} style={{marginBottom:"8px"}}>Criar</Button>
             {/* FIXME: default size for table */}
             <Table style={{height:"30vh"}}columns={columns} pagination={false} dataSource={data}/>
 
-            <Modal title="Adicionar uma empresa à tabela"
+            <Modal destroyOnClose 
+                   title={title}
                    open={open}
                    onOk={handleOk}
                    confirmLoading={confirmLoading}
                    onCancel={handleCancel}
-            >
-            {/* // TODO: insert forms and inputs*/}
-            {modalText}
-          
+                   afterClose={resetFields}
+                   >
+
+              <Form layout="vertical">
+                <FormItem label={"Nome"}>
+                  <Input placeholder="Nome de empresa" defaultValue={infoName}/>
+                </FormItem>
+
+                <FormItem label={"Informações"}>
+                  <TextArea placeholder="Informações sobre empresa" autoSize={{minRows: 3, maxRows: 5}} defaultValue={infoInfo}/>
+                </FormItem>
+
+                <FormItem label={"URL"}>
+                  <Input placeholder="e.g. https://empresa.com" type="url" defaultValue={infoURL}/>
+                </FormItem>
+              </Form>
             </Modal>
 
           </div>
 
         </div>
       </Content>
-      <Footer style={{height:"5vh",textAlign:"center"}}>porque eu programo {new Date().getFullYear()} Created by <a href="https://github.com/assebc" target="_blank">@assebc</a>  and  <a href="https://github.com/galleonpt" target="_blank">@galleonpt</a>.</Footer>
+      <Footer style={{textAlign:"center", height:"5vh", bottom:"0", width:"100%"}}>
+        porque eu programo {new Date().getFullYear()} Created by <a href="https://github.com/assebc" target="_blank">@assebc</a>  and  <a href="https://github.com/galleonpt" target="_blank">@galleonpt</a>.
+      </Footer>
     </Layout>
   );
 };
