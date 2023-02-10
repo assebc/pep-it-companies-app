85d8b47e6ea1b843d007d26853dcf90499c59df7
assebc <claudioafbessa@gmail.com>
2023-02-10 21:38:57 +0000

    feat: login  page

---
+++ frontend/src/components/Login/index.tsx
@@ -0,0 +1,85 @@
+import { FC, useState } from "react";
+import { Link , useNavigate } from "react-router-dom";
+import { Space, Button, Form, Input } from "antd";
+import api from "../../services/api";
+import "./styles.css";
+
+export const Login: FC = () => {
+  const [form] = Form.useForm();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [user, setUser] = useState<any>(); //FIXME: dont know type name
+  const navigate = useNavigate();
+  const isAdmin: boolean = localStorage.getItem("token") ? true : false;
+  
+  const handleSubmit = async (data: any) => { //FIXME: data its not any type 
+    // setUsername(data.username)
+    // setPassword(data.password);
+
+    // try{
+    //   const response = await api.get(`users/${username}`, {
+    //     headers: {
+    //       Authorization: `Bearer ${localStorage.getItem("token")}`,
+    //     },
+    //   });
+
+    //   if (response.status == 200 && password == response.data.password){
+    //     setUser(response.data);
+    //     alert("Login efetuado com sucesso!");
+    //     navigate("/companies");
+    //   }
+
+    // } catch (err: any){
+    //   alert(err.response.data.error);
+    // }
+    navigate("/companies");
+    
+  };
+
+  return (
+    <div className="login">
+      <h1>{"Login"}</h1>
+      <Form
+        layout="vertical"
+        form={form}
+        onFinish={handleSubmit}
+        className={"logininputs"}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Campo obrigatório" }]}
+          >
+            <Input placeholder="Username"/>
+          </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Campo obrigatório" }]}
+        >
+          <Input.Password placeholder="Password"/>
+        </Form.Item>
+
+        <Form.Item>
+          <Link to={"/changepassword"} className="forgotpwd">
+            Esqueceu-se da password? Clique aqui!
+          </Link>
+        </Form.Item>
+
+        <Form.Item>
+          <Space>
+            <Button type="default" className="invbutton" onClick={() => navigate("/companies")}>
+              Visitar
+            </Button>
+            <Button type="primary" className="button" htmlType="submit" >
+              Login
+            </Button>
+          </Space>
+        </Form.Item>
+
+      </Form>
+    </div>
+  );
+};
