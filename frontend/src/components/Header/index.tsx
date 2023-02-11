66a38aa51a516666a069f272496c29932f4a10fb
assebc <claudioafbessa@gmail.com>
2023-02-11 18:38:31 +0000

    feat: logout implementation

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,15 +1,29 @@
 import { FC } from "react";
-import { Layout } from "antd";
+import { Button, Layout } from "antd";
 import logo from "../../assets/pep-logo.png";
 import "./index.css";
+import { Link } from "react-router-dom";
 
 export const Header: FC = () => {
   return (
-    <Layout.Header className="header">
-      <a href="https://www.twitch.tv/porqueeuprogramo" target="_blank">
-        <img src={logo} alt="PEP LOGO"/>
-      </a>
+    localStorage.getItem("token") ?
+
+    <Layout.Header className="header_admin">
+      <Link to="https://www.twitch.tv/porqueeuprogramo" target="_blank">
+        <img width={"100px"} height={"100px"} src={logo} alt="PEP LOGO"/>
+      </Link>
       <p>PorqueEuProgramo • IT Companies</p>
-    </Layout.Header>
+      <Button className="logout" type="link">Logout</Button>
+    </Layout.Header>   
+      
+    :
+
+    <Layout.Header className="header">
+      <Link to="https://www.twitch.tv/porqueeuprogramo" target="_blank">
+        <img width={"100px"} height={"100px"} src={logo} alt="PEP LOGO"/>
+      </Link>
+      <p>PorqueEuProgramo • IT Companies</p>
+    </Layout.Header> 
+
   );
 };
