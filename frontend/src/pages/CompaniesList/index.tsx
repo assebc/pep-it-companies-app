c33287a4d697c47ea3aaed8f3682b4b5326aeaa5
assebc <claudioafbessa@gmail.com>
2023-02-20 22:06:20 +0000

    feat: delete button in frontend misses db connection

--- frontend/src/pages/CompaniesList/index.tsx
+++ frontend/src/pages/CompaniesList/index.tsx
@@ -31,7 +31,7 @@ export const CompaniesList: FC = () => {
       title: "Website",
       dataIndex: "website_url",
       key: "website_url",
-      width: "250px",
+      width: "220px",
       render: (url) => (
         <Link to={url} target="_blank">
           {url}
@@ -51,16 +51,29 @@ export const CompaniesList: FC = () => {
     {
       title: "Ações",
       key: "actions",
-      width: "180px",
+      width: "212px",
       render: (_, record) => {
         return (
-          <Space size="middle">
-            <Button onClick={() => handleVote(record)} children="Votar" />
-            <Button
-              onClick={() => navigate(`/companies/${record.id}`)}
-              children="Editar"
+          <>
+            <Button 
+              onClick={() => handleVote(record)} 
+              children="Votar" 
+              styles={{
+                width: "100%",
+                marginBottom: "8px"
+              }}
             />
-          </Space>
+            <Space size="middle">
+              <Button
+                onClick={() => navigate(`/companies/${record.id}`)}
+                children="Editar"
+              />
+              <Button
+                onClick={() => handleRemove(record)}
+                children="Remover"
+              />
+            </Space>
+          </>
         );
       },
     },
@@ -71,6 +84,10 @@ export const CompaniesList: FC = () => {
       setCompanies(response.data);
     });
   };
+
+  const removeCompany = (record : ICompany) => {
+    api.delete(`companies/${record.id}`);
+  }
 
   useEffect(() => {
     getCompanies();
@@ -85,6 +102,21 @@ export const CompaniesList: FC = () => {
       });
       if (response.status === 200) {
         getCompanies();
+      }
+    } catch (err: any) {
+      alert(err.response.data.error);
+    }
+  };
+
+  const handleRemove = async (record: ICompany) => {
+    try {
+      const response = await api.delete(`companies/${record.id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN_KEY)}`,
+        },
+      });
+      if (response.status === 200) {
+        removeCompany(record);
       }
     } catch (err: any) {
       alert(err.response.data.error);
