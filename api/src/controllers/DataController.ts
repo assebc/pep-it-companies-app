e0140a034231f2d669c503ffb178a93385d4d440
galleonpt <jose15.luis00@gmail.com>
2023-02-22 22:27:34 +0000

    feat: add route to download md file

--- api/src/controllers/DataController.ts
+++ api/src/controllers/DataController.ts
@@ -1,6 +1,8 @@
+import { Company } from "@prisma/client";
 import { Request, Response } from "express";
 import { prisma } from "../prisma";
-const data: any[] = require("../../companies.json");
+import { writeFile } from "fs";
+const data: any[] = require("../../import.json");
 
 class DataController {
   static formatData = () => {
@@ -25,6 +27,29 @@ class DataController {
     });
 
     return response.status(201).json({ created }).end();
+  }
+
+  async download(_: Request, response: Response) {
+    const companies: Partial<Company>[] = await prisma.company.findMany({
+      where: { deleted: false },
+      select: { name: true, reviews: true, website_url: true, votes: true },
+    });
+
+    const fileHeader = `# pep-it-portugal-companies\n\nIT Companies Portugal\n\n`;
+    const tableHeader = `| EMPRESAS | INFORMAÇÕES | WEBSITE | VOTAÇÕES |\n| --- | --- | --- | --- |\n`;
+    let tableData: string = "";
+
+    companies.forEach(
+      (company: Partial<Company>) =>
+        (tableData += `| ${company.name} | ${company.reviews} | [LINK](${company.website_url}) | ${company.votes}\n`)
+    );
+    const file = `${fileHeader} ${tableHeader} ${tableData}`;
+
+    writeFile(`companies_${Date.now()}.md`, file, (err) => {
+      if (err) console.log(err);
+    });
+
+    return response.json({ message: "Ficheiro criado com sucesso" }).end();
   }
 }
 
